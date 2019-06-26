6a9f93910b9744e22e36724d796d053e0fa331c4
clarence <1057697328@qq.com>
2019-06-26 15:33:14 +0800

    提交订单Mapper

---
+++ src/main/java/com/zilansw/zilanshop/dao/ZOrderDao.java
@@ -0,0 +1,21 @@
+package com.zilansw.zilanshop.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zilansw.zilanshop.pojo.ZOrder;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author : clarence
+ * 创建时间 : 2019-06-26
+ * 长沙轻翼网络科技有限公司
+ */
+@Mapper
+public interface ZOrderDao extends BaseMapper<ZOrder> {
+    /**
+     * 添加订单信息
+     * @param order
+     * @return
+     */
+    Integer addOrder(@Param("order") ZOrder order);
+}
